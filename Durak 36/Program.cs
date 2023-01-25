17d62533e7d579798f66c389b9707c7abc9c59e1
unknown <danenkov.dmitriy@list.ru>
2023-01-25 14:36:27 +0300

    1

---
+++ Durak 36/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using Durak_36;
+
+Console.WriteLine("Enter the number of cards to pick: ");
+string line = Console.ReadLine();
+if (int.TryParse(line, out int numberOfCards))
+{
+    CardPicker.PickSomeCards(numberOfCards);
+    foreach(string card in CardPicker.PickSomeCards(numberOfCards))
+    {
+        Console.WriteLine(card);
+    }
+}
+else
+{
+    Console.WriteLine("Enter a valid number");
+}
